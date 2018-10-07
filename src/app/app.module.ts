b4ea132768e5978d88a48c432823e27a1eac7a65
LAPTOP-0OU6KQPV\CINTELLIGENCE <prathim007@gmail.com>
2018-10-07 20:37:42 +0530

    Invitation

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgxCarouselModule } from 'ngx-carousel';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+	FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    NgxCarouselModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
